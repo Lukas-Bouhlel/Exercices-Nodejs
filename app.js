74c30ed40ea5cb4864447bea263d8340731667f4
Lukas Bouhlel <lukas.bouhlel@epitech.eu>
2024-03-19 15:29:44 +0100

    add router

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
+const router = require("./app/routes/index.js");
 
 const db = require("./app/models/index.js");
 db.sequelize
 .authenticate()
 .then(() => console.log("Database connected ..."))
 .catch((err) => console.log(err));
+
+app.use("/api", router);
 
 module.exports = app;
