5c1d835edb06eceba531f77cc9cc1564914800ae
Lukas Bouhlel <lukas.bouhlel@epitech.eu>
2024-03-19 11:11:32 +0100

    add-models

---
+++ app/models/wood.js
@@ -0,0 +1,31 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Wood extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Wood.init({
+    name: DataTypes.STRING,
+    type: {
+      type: DataTypes.ENUM('softwood', 'exotic wood', 'noble and hardwoods'),
+      allowNull: false,
+    },
+    hardness: {
+      type: DataTypes.ENUM('tender', 'medium-hard', 'hard'),
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'Wood',
+  });
+  return Wood;
+};
