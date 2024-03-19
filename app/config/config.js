d17b0c7ebbe3b8b1cdf70d90a0c400ffb412ef7d
Lukas Bouhlel <lukas.bouhlel@epitech.eu>
2024-03-19 12:03:23 +0100

    update env db connected

---
+++ app/config/config.js
@@ -0,0 +1,33 @@
+require('dotenv').config(); // Charge les variables d'environnement depuis .env
+
+const {
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_DATABASE,
+  DB_HOST,
+  NODE_ENV
+} = process.env;
+
+module.exports = {
+  development: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST,
+    dialect: 'mysql'
+  },
+  test: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_DATABASE}_test`,
+    host: DB_HOST,
+    dialect: 'mysql'
+  },
+  production: {
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: `${DB_DATABASE}_prod`,
+    host: DB_HOST,
+    dialect: 'mysql'
+  }
+};
