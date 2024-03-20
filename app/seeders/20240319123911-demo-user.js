d4d9ddd1901393d19b4a16c89e9dfc653b27980c
Lukas Bouhlel <lukas.bouhlel@epitech.eu>
2024-03-20 10:20:21 +0100

    Update 20240319123911-demo-user.js

--- app/seeders/20240319123911-demo-user.js
+++ app/seeders/20240319123911-demo-user.js
@@ -1,14 +1,13 @@
 'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
 const bcrypt = require('bcrypt');
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [{
       firstName: 'John',
       lastName: 'Doe',
       email: 'john.doe@mail.com',
-      password: await bcrypt.hash('123456', 10),
+      password: bcrypt.hashSync('123456', 10),
       createdAt: new Date(),
       updatedAt: new Date()
     },
@@ -16,7 +15,7 @@ module.exports = {
       firstName: 'Jane',
       lastName: 'Smith',
       email: 'jane.smith@mail.com',
-      password: await bcrypt.hash('password123', 10),
+      password: bcrypt.hashSync('password123', 10),
       createdAt: new Date(),
       updatedAt: new Date()
     },
@@ -24,7 +23,7 @@ module.exports = {
       firstName: 'Alice',
       lastName: 'Johnson',
       email: 'alice.johnson@mail.com',
-      password: await bcrypt.hash('securepassword', 10),
+      password: bcrypt.hashSync('securepassword', 10),
       createdAt: new Date(),
       updatedAt: new Date()
     }
