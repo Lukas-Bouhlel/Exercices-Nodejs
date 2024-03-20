38eff84cc3e3da9b09fe2e027326c4b9255ab9a2
Lukas Bouhlel <lukas.bouhlel@epitech.eu>
2024-03-20 10:01:18 +0100

    list wood

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,3 +1,12 @@
-exports.list = (req, res) => {
-    res.send('List of woods');
+const { Wood } = require('../models');
+
+exports.readAll = async (req, res) => {
+    try {
+        const woods = await Wood.findAll()
+        res.status(200).json(woods);
+    } catch(error) {
+        res.status(500).json({ 
+            message: error.message || 'Could not read all wood' 
+        });
+    }
 }
