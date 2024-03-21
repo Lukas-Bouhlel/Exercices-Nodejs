4b886eb3ba4ceb29a5bdad25b742b66d7102ed71
Lukas Bouhlel <lukas.bouhlel@epitech.eu>
2024-03-21 16:53:56 +0100

    add hateoas

--- app/controllers/wood.js
+++ app/controllers/wood.js
@@ -1,13 +1,18 @@
 const { Wood } = require('../models');
 const fs = require('fs');
+const { hateoasifyWood, hateoasifyCollectionWood } = require('../helpers/hateoas');
 
 exports.createWood = async (req, res) => {
     try {
         const pathname = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
-        const newWood = await Wood.create({
+        let newWood = await Wood.create({
             ...JSON.parse(req.body.datas),
             image: pathname,
         });
+        newWood = {
+            ...newWood.toJSON(),
+            links: hateoasifyCollectionWood(newWood.id, newWood.hardness),
+        };
         res.status(201).json(newWood);
     } catch (error) {
         res.status(500).json({
@@ -26,15 +31,15 @@ exports.updateWood = async (req, res) => {
             })
         }
 
-        let newWood = {
+        let updateWood = {
             ...JSON.parse(req.body.datas)
         }
 
         if (req.file) {
             const pathname = `${req.protocol}://${req.get("host")}/uploads/${req.file.filename}`;
-            
-            newWood = {
-                ...newWood,
+
+            updateWood = {
+                ...updateWood,
                 image: pathname
             };
 
@@ -50,7 +55,12 @@ exports.updateWood = async (req, res) => {
             }
         }
 
-        await wood.update(newWood);
+        await wood.update(updateWood);
+
+        wood = {
+            ...wood.toJSON(),
+            links: hateoasifyCollectionWood(wood.id, wood.hardness)
+        };
 
         res.status(200).json(wood);
     } catch (error) {
@@ -70,7 +80,7 @@ exports.deleteWood = async (req, res) => {
             })
         }
 
-        if(wood.image) {
+        if (wood.image) {
             const filename = wood.image.split("/uploads/")[1];
             fs.unlink(`uploads/${filename}`, (err) => {
                 if (err) {
@@ -80,7 +90,7 @@ exports.deleteWood = async (req, res) => {
                 }
             });
         }
-        
+
         await wood.destroy(wood);
 
         res.status(204).send();
@@ -93,8 +103,14 @@ exports.deleteWood = async (req, res) => {
 
 exports.readAll = async (req, res) => {
     try {
-        const woods = await Wood.findAll()
-        res.status(200).json(woods);
+        let woods = await Wood.findAll();
+        let woodsWithLinks = woods.map(wood => {
+            return {
+                ...wood.toJSON(),
+                links: hateoasifyCollectionWood(wood.id, wood.hardness),
+            };
+        });
+        res.status(200).json({ woodsWithLinks, links: hateoasifyWood() });
     } catch (error) {
         res.status(500).json({
             message: error.message || 'Could not read all wood'
@@ -106,7 +122,13 @@ exports.findByHardness = async (req, res) => {
     const { hardness } = req.params;
     try {
         const woods = await Wood.findAll({ where: { hardness } });
-        res.status(200).json(woods);
+        let woodsWithLinks = woods.map(wood => {
+            return {
+                ...wood.toJSON(),
+                links: hateoasifyCollectionWood(wood.id, wood.hardness),
+            };
+        });
+        res.status(200).json({ woodsWithLinks, links: hateoasifyWood() });
     } catch (error) {
         res.status(500).json({
             message: error.message || 'Could not find woods by hardness'
---
+++ app/helpers/hateoas.js
@@ -0,0 +1,44 @@
+exports.hateoasifyWood = () => {
+    return [
+        { 
+            rel: "readall", 
+            href: "/api/wood/", 
+            method: "GET" 
+        },
+        { 
+            rel: "by hardness", 
+            href: "/api/wood/:hardness", 
+            method: "GET" 
+        },
+        { 
+            rel: "create", 
+            href: "/api/wood/create", 
+            method: "POST" 
+        }
+    ];
+}
+
+exports.hateoasifyCollectionWood = (woodId, hardness) => {
+    return [
+        { 
+            rel: "self", 
+            href: `/api/wood/${woodId}`, 
+            method: "GET" 
+        },
+        { 
+            rel: "edit", 
+            href: `/api/wood/${woodId}`, 
+            method: "PUT" 
+        },
+        { 
+            rel: "delete", 
+            href: `/api/wood/${woodId}`, 
+            method: "DELETE" 
+        },
+        { 
+            rel: "sameHardness", 
+            href: `/api/wood/hardness/${hardness}`, 
+            method: "GET" 
+        }
+    ];
+}
